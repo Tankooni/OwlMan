b9b5a8b4537128936db8cba387d2fbe06e36bb17
Sam De Lara <samrdelara@gmail.com>
2023-12-05 21:18:09 -0800

    Options & quit button implemented

--- OwlMan/Scripts/TitleScreen.gd
+++ OwlMan/Scripts/TitleScreen.gd
@@ -7,3 +7,7 @@ func _on_start_button_pressed():
 	print("Start Button Pressed!")
 	var parameters: Dictionary = {"test": "f"}
 	Functions.load_screen_to_scene("res://scenes/world_select/world_select.tscn", parameters)
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
