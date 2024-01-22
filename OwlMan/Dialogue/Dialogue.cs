064c088ad247abd9b28c10c51963980f76f32272
Sam De Lara <samrdelara@gmail.com>
2024-01-21 23:30:23 -0800

    Added progress on importing the JSON file

--- OwlMan/Dialogue/Dialogue.cs
+++ OwlMan/Dialogue/Dialogue.cs
@@ -1,8 +1,33 @@
 using Godot;
 using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class DialogueEntry
+{
+	public string ID { get; set; }
+	public string Character { get; set; }
+	public string Pose { get; set; }
+	public string Text { get; set; }
+	public string Options { get; set; }
+	public string EmitSignal { get; set; }
+	public string WaitSeconds { get; set; }
+	public string Function { get; set; }
+	public string If { get; set; }
+	public string Add { get; set; }
+}
+
+public class DialogueData
+{
+	public List<DialogueEntry> Dialogues { get; set; }
+}
 
 public partial class Dialogue : CanvasLayer
 {
+
+	private DialogueData dialogueData;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -39,7 +64,52 @@ public partial class Dialogue : CanvasLayer
 
 	public void ParseJSON(string IDLabel)
 	{
-		GD.Print("Going to parse JSON label for :" + IDLabel);
+		GD.Print("Going to parse JSON label for : " + IDLabel);
+		ReadJSONFile(IDLabel);
+		GD.Print("Finished");	
+	}
+
+
+	public void ReadJSONFile(string IDLabel)
+	{
+		GD.Print("Starting READ function");
+		// Specify the path to your JSON file
+		string filePath = "res://Dialogue/Dialogue.json";
+
+		//GD.Print(filePath);
+
+		String Data = ObtainFileString(filePath);
+
+		//GD.Print(Data);
+		dialogueData = JsonSerializer.Deserialize<DialogueData>(Data);	
+
+		GD.Print("DIALOGUEDATA");
+		GD.Print(dialogueData.Dialogues);
+
+
+		DialogueEntry entry = GetDialogueEntryByID(IDLabel);
+
+
+// Use entry.infos to customize the dialog scene
+		if (entry != null)
+		{
+			GD.Print(entry.Text);
+		}
+
+	}
+	
+
+	private string ObtainFileString(string path)
+	{
+		var file = Godot.FileAccess.Open(path, Godot.FileAccess.ModeFlags.Read); //Readonly
+		string result = file.GetAsText();
+		file.Close();
+		return result;
+	}
+
+	private DialogueEntry GetDialogueEntryByID(string targetID)
+	{
+		return dialogueData.Dialogues.Find(entry => entry.ID == targetID);
 	}
 
 }
